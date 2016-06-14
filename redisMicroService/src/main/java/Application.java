0285dbcc9a63377e39b889dd52d2ba1e1befbcc6
alex <xalexpintax@gmail.com>
2016-06-15 00:25:07 +0300

    initial commit

---
+++ redisMicroService/src/main/java/Application.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Spring Boot main bootstrap.Application
+ *
+ */
+@Configuration @EnableAutoConfiguration @EnableWebMvc
+@ComponentScan(basePackages = { "controller", "helper" })
+@PropertySource("classpath:/settings/application.properties")
+public class Application {
+    public static void main( String[] args ) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
